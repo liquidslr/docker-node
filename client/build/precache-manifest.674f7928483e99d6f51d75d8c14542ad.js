7bdb2f268c577d8d0e036d4e29e8d4a1b0362c95
Gaurav Kumar <gaurav.kumar9825@gmail.com>
2018-12-23 20:28:18 +0530

    initail commit

---
+++ client/build/precache-manifest.674f7928483e99d6f51d75d8c14542ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bf19b3164cc3728ea66d",
+    "url": "/static/js/main.bf19b316.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "bf19b3164cc3728ea66d",
+    "url": "/static/css/main.3dfe48a6.chunk.css"
+  },
+  {
+    "revision": "4d218a15c81c599524f16fd179db8247",
+    "url": "/index.html"
+  }
+];
